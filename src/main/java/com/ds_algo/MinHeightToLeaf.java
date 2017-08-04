8913be58b31e2f5299a9179c4886c7cd54644ac5
Nilesh Kumar <nilesh.kumar@snapdeal.com>
2017-08-04 16:59:34 +0530

    Re-generate JavaDoc

--- src/main/java/com/ds_algo/MinHeightToLeaf.java
+++ src/main/java/com/ds_algo/MinHeightToLeaf.java
@@ -4,6 +4,8 @@ import org.jetbrains.annotations.Contract;
 
 /**
  * Created by nilesh.kumar on 04/08/17.
+ * <p>
+ * Compute minimum height of a node to leaf
  *
  * @author nilesh.kumar
  * @version 1.0-SNAPSHOT
@@ -13,10 +15,6 @@ import org.jetbrains.annotations.Contract;
  * @since 1.0-SNAPSHOT
  * <p>
  * <p>
- * Find the maximum sum leaf to root path in a Binary Tree
- * <p>
- * Given a Binary Tree, find the maximum sum path from a leaf to root. For example, in the following tree, there are three leaf to root paths 8->-2->10, -4->-2->10 and 7->10.
- * The sums of these three paths are 16, 4 and 17 respectively. The maximum of them is 17 and the path for maximum is 7->10.
  */
 public class MinHeightToLeaf extends BT {
     private Node n;
